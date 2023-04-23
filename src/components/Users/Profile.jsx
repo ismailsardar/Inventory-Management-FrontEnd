3a632a8f3e0acba9a6ebfecf3be03dbda39a2cb6
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-23 06:26:22 +0600

    update Profile component

---
+++ src/components/Users/Profile.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Profile = () => {
+  let navigate = useNavigate();
+  return (
+    <div className="container">
+      <div className="row d-flex justify-content-center">
+        <div className="col-md-12">
+          <div className="card">
+            <div className="card-body">
+              <div className="container-fluid">
+                <img
+                  //   ref={(input) => (userImgView = input)}
+                  className="icon-nav-img-lg"
+                  //   src={ProfileData["photo"]}
+                  alt=""
+                />
+                <hr />
+                <div className="row">
+                  <div className="col-4 p-2">
+                    <label>Profile Picture</label>
+                    <input
+                      //   onChange={PreviewImage}
+                      //   ref={(input) => (userImgRef = input)}
+                      placeholder="User Email"
+                      className="form-control animated fadeInUp"
+                      type="file"
+                    />
+                  </div>
+                  <div className="col-4 p-2">
+                    <label>Email Address</label>
+                    <input
+                      key={Date.now()}
+                      defaultValue="email"
+                      //   defaultValue={ProfileData["email"]}
+                      readOnly={true}
+                      //   ref={(input) => (emailRef = input)}
+                      placeholder="User Email"
+                      className="form-control animated fadeInUp"
+                      type="email"
+                    />
+                  </div>
+                  <div className="col-4 p-2">
+                    <label>First Name</label>
+                    <input
+                      key={Date.now()}
+                      defaultValue="firstName"
+                      //   defaultValue={ProfileData["firstName"]}
+                      //   ref={(input) => (firstNameRef = input)}
+                      placeholder="First Name"
+                      className="form-control animated fadeInUp"
+                      type="text"
+                    />
+                  </div>
+                  <div className="col-4 p-2">
+                    <label>Last Name</label>
+                    <input
+                      key={Date.now()}
+                      defaultValue="lastName"
+                      //   defaultValue={ProfileData["lastName"]}
+                      //   ref={(input) => (lastNameRef = input)}
+                      placeholder="Last Name"
+                      className="form-control animated fadeInUp"
+                      type="text"
+                    />
+                  </div>
+                  <div className="col-4 p-2">
+                    <label>Mobile</label>
+                    <input
+                      key={Date.now()}
+                      //   defaultValue={ProfileData["mobile"]}
+                      defaultValue="01968795466"
+                      //   ref={(input) => (mobileRef = input)}
+                      placeholder="Mobile"
+                      className="form-control animated fadeInUp"
+                      type="mobile"
+                    />
+                  </div>
+                  <div className="col-4 p-2">
+                    <label>Password</label>
+                    <input
+                      key={Date.now()}
+                      defaultValue="password"
+                      //   defaultValue={ProfileData["password"]}
+                      //   ref={(input) => (passwordRef = input)}
+                      placeholder="User Password"
+                      className="form-control animated fadeInUp"
+                      type="password"
+                    />
+                  </div>
+                  <div className="col-4 p-2">
+                    <button
+                      //   onClick={UpdateMyProfile}
+                      className="w-100  btn btn-success"
+                    >
+                      Update
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
