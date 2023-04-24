addabd31f1e043f86e6c165e5e5739b230bae6f2
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-24 10:10:02 +0600

    update FullscreenLoader component

--- src/components/MasterLayout/FullscreenLoader.jsx
+++ src/components/MasterLayout/FullscreenLoader.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const FullscreenLoader = () => {
+  const loader = useSelector((state) => state.settings.loader);
   return (
-    <div>FullscreenLoader</div>
-  )
-}
+    <>
+      <div className={loader + " LoadingOverlay"}>
+        <div className="Line-Progress">
+          <div className="indeterminate"></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default FullscreenLoader
+export default FullscreenLoader;
