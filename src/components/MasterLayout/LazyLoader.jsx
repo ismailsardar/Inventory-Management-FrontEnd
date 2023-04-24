b560313d510b4fc83900d4cd109a0cafcde0dcb7
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-24 10:09:37 +0600

    update LazyLoader components

--- src/components/MasterLayout/LazyLoader.jsx
+++ src/components/MasterLayout/LazyLoader.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 const LazyLoader = () => {
   return (
-    <div className="LoadingOverlay">
+    <div className={"LoadingOverlay d-none"}>
       <div className="Line-Progress">
-        <div className="indeterminate" />
+        <div className="indeterminate"></div>
       </div>
     </div>
   );
