2f51454f80f1f436f32fa1dfef9d0c82064a85f1
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-27 15:40:38 +0600

    update SupplierList

--- src/components/Supplier/SupplierList.jsx
+++ src/components/Supplier/SupplierList.jsx
@@ -88,7 +88,7 @@ const SupplierList = () => {
                         <option value="30">30 Per Page</option>
                         <option value="50">50 Per Page</option>
                         <option value="100">100 Per Page</option>
-                        <option value="100">200 Per Page</option>
+                        <option value="200">200 Per Page</option>
                       </select>
                     </div>
                     <div className="col-4">
